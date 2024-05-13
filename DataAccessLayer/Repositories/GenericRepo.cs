41de39cac8a25fb7106b6fc9c085681452e68f5a
Mohamed Selmy <mohammed_en30894528@f-eng.tanta.edu.eg>
2024-05-13 23:25:09 +0300

    Implement StudentRepo

---
+++ DataAccessLayer/Repositories/GenericRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Data;
+
+namespace BusinessLogicLayer.Repositories
+{
+    internal interface IGenericRepo<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void SaveChanges();
+    }
+
+    StaticDataContext
+}
---
+++ DataAccessLayer/Repositories/StudentRepo.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogicLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Repositories
+{
+    internal class StudentRepo : GenericRepo<Student>
+    {
+        
+
+        public void Add(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Student> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Student entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
